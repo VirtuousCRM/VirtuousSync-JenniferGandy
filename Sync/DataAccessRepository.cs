b4ed82a94b075f58541dabb06c010a7bf7a9ea86
JGandy <jennifergandy56@gmail.com>
2025-01-18 18:28:59 -0700

    feat: adding simple db save logic to replace existing csv logic

---
+++ Sync/DataAccessRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using System.Threading.Tasks;
+
+namespace Sync
+{
+    public class DataAccessRepository
+    {
+        private readonly string _connectionString;
+
+        public DataAccessRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task SaveResults(IEnumerable<AbbreviatedContact> results)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                const string sql = "INSERT INTO contacts (Id, Name, ContactType, ContactName, Address, Email, Phone)" +
+                    "Values (@Id, @Name, @ContactType, @ContactName, @Address, @Email, @Phone)";
+
+                await connection.ExecuteAsync(sql, results);
+            }
+        }
+    }
+}
